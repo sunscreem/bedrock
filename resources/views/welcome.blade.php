63397edddcd4590a3c5c1e44524743d6dd147db0
Rob Cooper <sunscreem@gmail.com>
2018-11-01 19:00:19 +0000

    composer updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,22 +2,7 @@
 
 @section('content')
 
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="title m-b-md">
-                Laravel
-            </div>
-
-            <div class="links">
-                <a href="https://laravel.com/docs">Documentation</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://nova.laravel.com">Nova</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-            </div>
-        </div>
-    </div>
+Main Content
 
 @endsection
 
