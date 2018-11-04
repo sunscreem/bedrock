e00551324b2a24874891965ddb48eb04a9a155ae
Rob Cooper <sunscreem@gmail.com>
2018-11-04 20:01:06 +0000

    added the new auto-load vuejs cleverness

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,13 +9,12 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const files = require.context('./', true, /\.vue$/i)
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+files.keys().map(key => {
+    return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
+})
+
 
 const app = new Vue({
     el: '#app'
