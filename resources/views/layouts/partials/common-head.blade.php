cafbe0df8bf493a1dc98bbeb8c300e691908b470
Rob Cooper <sunscreem@gmail.com>
2019-01-22 19:10:35 +0000

    added extract of vendor

--- resources/views/layouts/partials/common-head.blade.php
+++ resources/views/layouts/partials/common-head.blade.php
@@ -8,5 +8,5 @@
         <title>{{ $pagetitle }}</title>
 
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-        <script src=" {{ mix( 'js/app.js') }}" defer></script>
+        
     </head>
---
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -0,0 +1,3 @@
+<script src=" {{ mix( 'js/manifest.js') }}" defer></script>
+<script src=" {{ mix( 'js/vendor.js') }}" defer></script>
+<script src=" {{ mix( 'js/app.js') }}" defer></script>
